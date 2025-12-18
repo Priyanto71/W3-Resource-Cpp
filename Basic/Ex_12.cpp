fdc8c1d9eb5221d1a6c98ddce76cd9fe100f4887
Priyanto Debnath <priyantodn71@gmail.com>
2025-12-18 19:31:32 +0600

    Add files via upload

---
+++ Basic/Ex_12.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int x,y;
+cout<<"Input 1st number : ";
+cin >> x;
+cout<<"Input 2nd number : ";
+cin >> y;
+cout << "The sum of the numbers is : "<<x+y;
+
+
+}
